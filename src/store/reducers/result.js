53c0f48b88e4f0616604200a509be0546f19f406
Russkazkin <ruslan@skazkin.su>
2021-03-09 16:41:23 +0300

    Move all logic from the reducers actions

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,13 +5,17 @@ const initialState = {
     results: [],
 };
 
+const deleteResult = (state, action) => {
+    const updatedResults = state.results.filter((result) => result.id !== action.id);
+    return updateObject(state, {results: updatedResults});
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})})
         case actionTypes.DELETE_RESULT:
-            const updatedResults = state.results.filter((result) => result.id !== action.id);
-            return updateObject(state, {results: updatedResults})
+            return deleteResult(state, action);
         default:
             return state;
     }
